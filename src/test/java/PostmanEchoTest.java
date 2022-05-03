0cac668d4f2ba7373dfc671410cf42748ebbea2c
Роман Шапарь <romanshaparution@gmail.com>
2022-05-03 14:39:44 +0300

    added

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void shouldSendRequestBody () {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+                .body("headers.content-length", equalTo("9"))
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("headers.content-type", equalTo("text/plain; charset=ISO-8859-1"))
+                .body("headers.user-agent", equalTo("Apache-HttpClient/4.5.3 (Java/11.0.13)"))
+        ;
+    }
+}
